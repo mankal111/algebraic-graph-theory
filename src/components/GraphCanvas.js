c28eaf000b9b1de4d715345aa8d6e6adeea5e88f
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-06 13:35:36 +0200

    handle select action  in vertex component

--- src/components/GraphCanvas.js
+++ src/components/GraphCanvas.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import { Layer, Stage } from 'react-konva';
 import Vertex from './Vertex';
-import {connect} from 'react-redux';
-import {addVertex} from '../actions/graphActions';
+import { connect } from 'react-redux';
+import { addVertex, selectVertex } from '../actions/graphActions';
 
 class GraphCanvas extends Component {
     constructor(...args) {
         super(...args);
         this.handleClick = this.handleClick.bind(this);
     }
+
     handleClick() {
         var {x, y} = this.refs.stage.getPointerPosition();
-        console.log(x,y)
         this.props.addVertex(x, y);
     }
+
     render() {
-        const vertices = this.props.vertices;
-        console.log(this.props.vertices)
+        const { vertices, selectedVertex, selectVertex } = this.props;
+
         return (
             <Stage width={window.innerWidth} height={window.innerHeight} onClick={this.handleClick} ref="stage" >
                 <Layer>
                     {
-                        vertices.map( (vertice, key) => 
+                        vertices.map( (vertice, index) => 
                             <Vertex
-                                key={key}
+                                key={index}
                                 x={vertice[0]}
                                 y={vertice[1]}
-                                index={key}
+                                index={index}
+                                selected={index === selectedVertex}
+                                selectVertex={selectVertex}
                             />
                         )
                     }
@@ -41,7 +44,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    addVertex: (x,y) => dispatch(addVertex(x,y))
+    addVertex: (x,y) => dispatch(addVertex(x,y)),
+    selectVertex: index => dispatch(selectVertex(index))
 });
   
 export default connect(mapStateToProps, mapDispatchToProps) (GraphCanvas);
