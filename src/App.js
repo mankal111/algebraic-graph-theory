221db80f157b1f599509076bb108ff49bd72dea9
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-07 21:24:12 +0200

    styled App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import GraphCanvas from './components/GraphCanvas';
 import GraphDataPanel from './components/GraphDataPanel.js';
@@ -10,9 +12,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Algebraic Graph Theory</h1>
-        <GraphDataPanel />
-        <GraphCanvas />
+        <Container>
+              <h1>Algebraic Graph Theory</h1>
+          <Row>
+            <Col md={8}>
+              <Jumbotron>
+                <GraphCanvas />
+              </Jumbotron>
+            </Col>
+            <Col md={4}>
+              <GraphDataPanel />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
