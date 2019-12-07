6a950e0d1523c751c924d45ae8b30a1e92a1559e
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-07 19:44:58 +0200

    created GraphDataPanel

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { connect } from 'react-redux';
 
 import './App.css';
 
-import GraphCanvas from './components/GraphCanvas'; 
+import GraphCanvas from './components/GraphCanvas';
+import GraphDataPanel from './components/GraphDataPanel.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Algebraic Graph Theory</h1>
+        <GraphDataPanel />
         <GraphCanvas />
       </div>
     );
