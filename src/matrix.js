13400e6876dcbdb59a86455f3dae6064c0e1d383
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-08 20:44:01 +0200

    determinant and characteristic polynomial

--- src/matrix.js
+++ src/matrix.js
@@ -1,3 +1,9 @@
+import { create, all } from 'mathjs';
+
+const config = { };
+const math = create(all, config);
+
+
 export const arrayToLatexMatrix = (array) =>
     `\\begin{vmatrix}${array.map(row => row.join('&')).join('\\\\')}\\end{vmatrix}`;
 
@@ -13,4 +19,57 @@ export const adjacencyMatrix = (numberOfVertices, arrayOfEdges) => {
         matrix[arrayOfEdges[i][1]][arrayOfEdges[i][0]] = 1;
     }
     return matrix;
+}
+
+export const minorMatrix = (array, i, j) => {
+    return array
+        // Get all the rows leaving out the ith
+        .filter((row, index) => index !== i)
+        // In each row get all elements leaving out the jth
+        .map(row => row.filter((el,index) => index !== j));
+}
+
+// This is not the fastest algorithm for finding the determinant
+export const determinant = (array) => {
+    if (array.length === 1) {
+        // In 1X1 the determinant is the unique element
+        return array[0][0];
+    } else if (array.length === 2) {
+        // In 2X2 the following is the determinant. We will use it as
+        // the first step in this recursive method.
+        return array[0][0]*array[1][1] - array[0][1]*array[1][0];
+    }
+    let det = 0;
+    for (let i = 0; i < array.length; i++){
+        det += Math.pow(-1, i)*array[0][i]*determinant(minorMatrix(array,0,i));
+    }
+    return det;
+}
+
+// Returns the determinant expression which will be used
+// for the computation of the characterestic polynomial
+export const determinantExpression = (array) => {
+    if (array.length === 1) {
+        // In 1X1 the determinant is the unique element
+        return array[0][0];
+    } else if (array.length === 2) {
+        // In 2X2 the following is the determinant. We will use it as
+        // the first step in this recursive method.
+        return `${array[0][0]}*${array[1][1]}-${array[0][1]}*${array[1][0]}`;
+    }
+    let det = '';
+    for (let i = 0; i < array.length; i++){
+        det += `${Math.pow(-1, i) < 0 ? '-' : '+'}*${array[0][i]}*(${determinant(minorMatrix(array,0,i))})`;
+    }
+    return math.evaluate(math.parse(det)).toString();
+}
+
+export const characteristicPolynomial = (array) => {
+    // Clone given array
+    let matrix = [...array];
+    // Add -l to the diagonal
+    for (let i=0; i < matrix.length; i++){
+        matrix[i][i] = 't';
+    }
+    return determinantExpression(matrix);
 }
--- src/matrix.test.js
+++ src/matrix.test.js
@@ -1,4 +1,5 @@
-import { arrayToLatexMatrix, zeros, adjacencyMatrix } from './matrix';
+import { arrayToLatexMatrix, zeros, adjacencyMatrix, characteristicPolynomial,
+    determinant, determinantExpression, minorMatrix } from './matrix';
 
 it('converts array to latex matrix', () => {
     expect(arrayToLatexMatrix([[1, 2],[3, 4]])).toEqual("\\begin{vmatrix}1&2\\\\3&4\\end{vmatrix}");
@@ -10,4 +11,24 @@ it('returns array of zeros', () => {
 
 it('returns the adjacency matrix of a graph', () => {
     expect(adjacencyMatrix(3,[[0,1],[0,2]])).toEqual([[0,1,1],[1,0,0],[1,0,0]]);
+});
+
+it('returns the minor matrix', () => {
+    expect(minorMatrix([[1,2],[3,4]], 0, 0)).toEqual([[4]]);
+    expect(minorMatrix([[1,2,3],[4,5,6],[7,8,9]], 1, 1)).toEqual([[1,3],[7,9]]);
+});
+
+it('returns the determinant', () => {
+    expect(determinant([[1,2],[3,4]])).toEqual(-2);
+    expect(determinant([[1,2,3],[4,5,6],[7,8,9]])).toEqual(0);
+    expect(determinant([[1,2,3],[1,5,6],[7,8,9]])).toEqual(-18);
+    expect(determinant([['l',1],[1,'l']])).toEqual(-2);
+});
+
+it('returns the determinant expression', () => {
+    expect(determinantExpression([['l',1],[1,'l']])).toEqual('l^2-1');
+});
+
+it('returns the characteristic polynomial of an array', () => {
+    expect(characteristicPolynomial([[0,1],[1,0]])).toEqual("t^2-1");
 });
