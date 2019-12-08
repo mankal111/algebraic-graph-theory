2d1020f57a6079482d418fa5997ff1a4d221d8c2
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-08 18:43:04 +0200

    typo

--- src/matrix.test.js
+++ src/matrix.test.js
@@ -8,6 +8,6 @@ it('returns array of zeros', () => {
     expect(zeros(2,3)).toEqual([[0,0,0],[0,0,0]]);
 });
 
-it('return the adjacency matrix of a graph', () => {
+it('returns the adjacency matrix of a graph', () => {
     expect(adjacencyMatrix(3,[[0,1],[0,2]])).toEqual([[0,1,1],[1,0,0],[1,0,0]]);
 });
