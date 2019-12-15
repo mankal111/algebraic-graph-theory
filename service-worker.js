0d941cd3841752e56b200e5ca12e3252a938e848
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-15 14:30:21 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/algebraic-graph-theory/precache-manifest.291bb0aae5eb15f80c412fd5e090f412.js"
+  "/algebraic-graph-theory/precache-manifest.b44c7caeef45975b1032707f7ccbf69f.js"
 );
 
 self.addEventListener('message', (event) => {
