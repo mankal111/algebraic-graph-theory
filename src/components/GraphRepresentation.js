8ac7c45ecf65650c49cf507c31f28ce52853f5ee
Manolis Kalafatis <mankal111@hotmail.com>
2019-12-17 12:05:32 +0200

    styled matrix scrollbars

--- src/components/GraphRepresentation.js
+++ src/components/GraphRepresentation.js
@@ -19,6 +19,17 @@ const useStyles = makeStyles(theme => ({
         width: '100%',
         maxHeight: '250px',
         overflow: 'auto',
+        '&::-webkit-scrollbar': {
+            width: '0.4em',
+            height: '0.4em',
+        },
+        '&::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+        },
+        '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,.1)',
+            outline: '1px solid slategrey'
+        },
     },
 }))
 
