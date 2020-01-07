ee4c47f043b557ca904bd8a1f381c4069ec29c54
Manolis Kalafatis <mankal111@hotmail.com>
2020-01-07 15:50:56 +0200

    added test cases

--- src/graph/graph.test.js
+++ src/graph/graph.test.js
@@ -11,24 +11,22 @@ it('Creates new Graph object using create() method', () => {
 });
 
 it('Initializes vertices', () => {
-    expect(Graph.create().initializeVertices(5).numberOfVertices()).toEqual(5);
+    let graph = Graph.create().initializeVertices(5);
+    expect(graph.numberOfVertices()).toEqual(5);
     expect(
-        Graph
-        .create()
+        graph
         .initializeVertices([{some: 'data'}, {some: 'otherData'}, {}])
         .numberOfVertices()
     ).toEqual(3);
+    expect(graph.initializeVertices().numberOfVertices()).toEqual(0);
 });
 
 it('Returns the number of edges', () => {
-    expect(
-        Graph
-        .create()
-        .initializeVertices(3)
-        .addEdge([0, 1])
-        .addEdge([1, 2])
-        .numberOfEdges()
-    ).toEqual(2);
+    let graph = Graph.create().initializeVertices(3);
+    expect(graph.addEdge([0, 1]).addEdge([1, 2]).numberOfEdges()).toEqual(2);
+    expect(() => graph.addEdge([1])).toThrow();
+    expect(() => graph.addEdge(['string', 1])).toThrow();
+    expect(() => graph.addEdge([1, 'string'])).toThrow();
 });
 
 it('Returns the number of vertices', () => {
